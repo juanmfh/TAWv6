6332aa1c5ea3449a45e3bea6a6b61088e497bbf7
olivo <olivo@olivo-portatil>
2013-06-13 16:21:31 +0200

    commit david

---
+++ Diputacion-war/src/java/diputacion/facturacion/CtrGestionComparativas.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diputacion.facturacion;
+
+
+import diputacion.dao.ComparativaFacadeLocal;
+import diputacion.entity.Comparativa;
+import java.io.Serializable;
+import java.util.Collection;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author David Dona Corrales
+ */
+@ManagedBean
+public class CtrGestionComparativas implements Serializable {
+
+    @EJB
+    private ComparativaFacadeLocal comparativaFacade;
+    private Collection<Comparativa> comparativas;
+    private Comparativa comparativaSelec;
+
+    public Comparativa getComparativaSelec() {
+        return comparativaSelec;
+    }
+
+    public void setComparativaSelec(Comparativa comparativaSelec) {
+        this.comparativaSelec = comparativaSelec;
+    }
+
+    public CtrGestionComparativas() {
+    }
+
+    public Collection<Comparativa> getComparativas() {
+        return comparativas;
+    }
+
+    public void setComparativas(Collection<Comparativa> comparativas) {
+        this.comparativas = comparativas;
+    }
+    
+    public String verComparativa(){
+        return "VistaVerComparativa";
+    }
+    
+    public String borrarComparativa(){
+        this.comparativaFacade.remove(comparativaSelec);
+        comparativas.remove(comparativaSelec);
+        return "VistaGestionComparativas";
+    }
+
+    @PostConstruct
+    public void create() {
+        comparativas = comparativaFacade.findAll();
+        
+        
+        
+    }
+}
