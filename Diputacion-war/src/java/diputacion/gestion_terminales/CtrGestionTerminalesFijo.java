8c4c3e2e8eaab69b4cadd7a4da2aac2af366cf6f
Joaquin <Joaquin@192.168.1.65>
2013-06-17 11:38:32 +0200

    Insertar y Borrar terminal fijo

--- Diputacion-war/src/java/diputacion/gestion_terminales/CtrGestionTerminalesFijo.java
+++ Diputacion-war/src/java/diputacion/gestion_terminales/CtrGestionTerminalesFijo.java
@@ -4,6 +4,10 @@
  */
 package diputacion.gestion_terminales;
 
+import diputacion.dao.LineafijaFacadeLocal;
+import diputacion.dao.LineamovilFacadeLocal;
+import diputacion.dao.TerminalfijoFacadeLocal;
+import diputacion.dao.UsuarioFacadeLocal;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -11,16 +15,29 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import diputacion.entity.Terminalfijo;
 import diputacion.entity.Usuario;
-
+import java.text.ParseException;
+import java.util.StringTokenizer;
+import javax.annotation.PostConstruct;
+import diputacion.entity.Lineafija;
+import java.io.Serializable;
+import javax.ejb.EJB;
 /**
  *
  * @author Joaquin
  */
 @ManagedBean(name = "ctrGestionTerminalesFijo")
 @SessionScoped
-public class CtrGestionTerminalesFijo {
+public class CtrGestionTerminalesFijo implements Serializable{
     
-   // private LineaFijaFacade lineaFijaFacade;
+    @EJB
+    private LineafijaFacadeLocal lineafijaFacade;
+    @EJB
+    private UsuarioFacadeLocal usuarioFacade;
+    @EJB
+    private TerminalfijoFacadeLocal terminalfijoFacade;
+    
+    
+   //VARIABLES
     private String marca, modelo, linea, fecha;
     private Date fechaAlta;
     private Collection<Terminalfijo> terminales;
@@ -139,6 +156,85 @@ public class CtrGestionTerminalesFijo {
     public void setTerminalSeleccionado(Terminalfijo tf) {
         terminalSeleccionado = tf;
     }
+    
+    //METODOS-------------------------------------------------------------------
+    @PostConstruct
+    public void inicializacion() {
+        terminales = terminalfijoFacade.findAll();
+        //OBTENEMOS LA FECHO DE SISTEMA
+        int dia = new java.util.Date().getDate();
+        int mes = new java.util.Date().getMonth() + 1;
+        int ano = new java.util.Date().getYear() + 1900;
+        //PONEMOS LA FECHA EN BLANCO
+        fecha = "";
+        fecha += dia + "/" + mes + "/" + ano;
+        marca = "";
+        modelo = "";
+        linea = "";
+        //RECOGEMOS LOS USUARIOS
+        usuarios = usuarioFacade.findAll();
+
+    }
+    
+    public String formularioInsertar() {
+        this.inicializacion();
+        return "FormularioInsertarFijo";
+    }
+
+    public String insertarTerminalFijo() throws ParseException {
+        //CREAMOS EL OBJETO TERMINAL FIJO
+        Terminalfijo tfnuevo;
+        Lineafija lf;
+        tfnuevo = new Terminalfijo();
+        tfnuevo.setMarca(marca);
+        tfnuevo.setModelo(modelo);
+
+        //SI LA LINEA NO ES VACIA INSERTAMOS UNA NUEVA LINEA EN EL SISTEMA   
+
+        //COMPROBAMOS SI EL CAMPO PUBLICO ESTA MARCADO
+        if (!publico.isEmpty()) {
+            pub = true;
+        }
+        if (linea.length() > 0) {
+            lf = new Lineafija();
+
+            //PASEAMOS LA FECHA
+            if (fecha.length() > 0) {
+                StringTokenizer tokens = new StringTokenizer(fecha, "/");;
+                int[] datos = new int[3];
+                int i = 0;
+                while (tokens.hasMoreTokens()) {
+                    String str = tokens.nextToken();
+                    datos[i] = Integer.parseInt(str);
+                    System.out.println(datos[i]);
+                    i++;
+                }
+                fechaAlta = new java.util.Date(datos[2] - 1900, datos[1] - 1, datos[0]);
+                lf.setFechaAlta(fechaAlta);
+            }
+
+            lf.setNumeroLinea(Integer.parseInt(linea));
+            lf.setPublico(pub);
+            lineafijaFacade.create(lf);
+            tfnuevo.setLineaFijaidlineaFija(lf);
+            pub = false;
+        }
+
+        //INSERTAMOS EN LA BD
+        terminalfijoFacade.create(tfnuevo);
+
+        this.inicializacion();
+        return "ListadoTerminalFijo";
+    }
+    
+    public void borrarTerminalFijo() {
+
+        terminalfijoFacade.remove(terminalSeleccionado);
+        this.inicializacion();
+
+    }
+
+
 
     
     
