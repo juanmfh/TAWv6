2b64dcda9ed1e6373deea395fee92fa1de546f25
Alex <>
2013-06-19 13:49:50 +0200

    Añadida opción para crear perfiles

--- Diputacion-war/src/java/diputacion/control_gasto/CtrVistaNuevoPerfil.java
+++ Diputacion-war/src/java/diputacion/control_gasto/CtrVistaNuevoPerfil.java
@@ -1,7 +1,10 @@
 package diputacion.control_gasto;
 
+import diputacion.dao.PerfilFacadeLocal;
+import diputacion.entity.Perfil;
 import java.io.Serializable;
-import javax.annotation.PostConstruct;
+import java.util.Date;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -15,13 +18,45 @@ public class CtrVistaNuevoPerfil implements Serializable {
     /**
      * Creates a new instance of CtrVistaNuevoPerfil
      */
-    
+    @EJB
+    private PerfilFacadeLocal perfilFacade;
+    private Date fechaFin = null;
+    private double limite = 0;
 
     public CtrVistaNuevoPerfil() {
     }
 
-    @PostConstruct
-    public void init() {
-        
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public String addPerfil() {
+        if (fechaFin == null || limite == 0) {
+            return "VistaNuevoPerfil";
+        } else {
+            Perfil pf = new Perfil();
+            pf.setFechaFin(fechaFin);
+            pf.setLimite(limite);
+            pf.setTarifaMovilidtarifaMovil(null);
+            perfilFacade.create(pf);
+
+            return "VistaAddPerfil";
+        }
+    }
+
+    public String addTarifa() {
+        return "VistaNuevaTarifa";
     }
 }
